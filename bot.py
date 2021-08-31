39dbc43faebce62711b2bf8fd3b4e02c113b0fb7
MOstafa Arshadi <mostafa1382ma@gmail.com>
2021-08-31 12:17:38 +0430

    add base

---
+++ bot.py
@@ -0,0 +1,22 @@
+from pyrogram import Client,types
+import uuid
+import logging
+
+DOMAIN = "http://127.0.0.1:5000"
+logging.basicConfig()
+client = Client("bot",'3069922','66b2a19c29c56c943c726af7200c41fd',bot_token="1877221140:AAHBauy1O1O0sXmE9RGtrzeZ6GsX7iIx_xU",
+proxy=dict(hostname="127.0.0.1",port=8010))
+
+
+
+@client.on_message()
+async def handler1(_,m:types.Message):
+    photo = m.photo
+    print(m)
+    if photo:
+        await client.download_media(m,f"files/{photo.file_id}.png")
+        await m.reply(f"Link: {DOMAIN}/{photo.file_id}.png")
+
+
+
+client.run()
---
+++ web.py
@@ -0,0 +1,27 @@
+from flask import Flask,send_file
+import os
+app = Flask(__name__)
+
+if not os.path.exists("files"):
+    os.mkdir("files")
+mime_type = {
+    "png":"image/png",
+    "jpg":"image/jpg",
+    "jpeg":"image/jpeg",
+}
+
+
+
+@app.route("/<file>")
+def home(file):
+    if os.path.exists(f"files/{file}"):
+        return send_file(f"files/{file}")
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT",5000)
+    app.run(host="0.0.0.0",port=port)
+
+
+
+
+
