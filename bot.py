fa740130bd052a8ff04248f72164d19348c6b976
MOstafa Arshadi <mostafa1382ma@gmail.com>
2021-08-31 12:24:34 +0430

    add base

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ DOMAIN = "https://file-to-link-ar.herokuapp.com"
 logging.basicConfig()
 client = Client("bot",'3069922','66b2a19c29c56c943c726af7200c41fd',bot_token="1877221140:AAHBauy1O1O0sXmE9RGtrzeZ6GsX7iIx_xU",
 #proxy=dict(hostname="127.0.0.1",port=8010)
-# )
+)
 
 
 
